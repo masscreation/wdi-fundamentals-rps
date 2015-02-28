f1871a44b5c1a57e9fcaef7237bbc26ddc8fd2b7
MassCreation <dennisb.bishop@gmail.com>
2015-02-27 16:46:55 -0800

    Add expressions to functions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -24,15 +24,27 @@ function randomPlay() {
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
+    if (move !== null) {
+        return move; 
+    } 
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    else {
+        return getInput(); 
+    };
+    
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
+    if (move !== null) {
+        return move; 
+    } 
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    else {
+        return randomPlay(); 
+    };
+    
 }
 
 function getWinner(playerMove,computerMove) {
